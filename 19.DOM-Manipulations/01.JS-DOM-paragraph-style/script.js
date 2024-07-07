fe84a07bd2b8f7aeaf5305407f490bb754bec321
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-07-08 00:57:26 +0300

    Added solution to task 01

---
+++ 19.DOM-Manipulations/01.JS-DOM-paragraph-style/script.js
@@ -0,0 +1,9 @@
+function restyle() {
+    const families = ['Times New Roman', 'Georgia', 'Garamond', 'Arial', 'Verdana', 'Helvetica', 'Courier New', 'Lucida Console', 'Monaco', 'Brush Script MT', 'Lucida Handwriting', 'Copperplate', 'Papyrus'];
+
+    const pElement = document.getElementById('text');    
+
+    pElement.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    pElement.style.fontFamily = families[Math.floor(Math.random() * families.length)];
+    pElement.style.fontSize = Math.floor((Math.random() * 32) + 10) + 'px';
+}
