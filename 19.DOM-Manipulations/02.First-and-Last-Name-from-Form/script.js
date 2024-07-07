f997c210229338179cd2f5afa6f5acd00e4f17a4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-07-08 01:24:21 +0300

    Added task 02 solution

---
+++ 19.DOM-Manipulations/02.First-and-Last-Name-from-Form/script.js
@@ -0,0 +1,7 @@
+function getFormvalue() {
+    const firstNameElement = document.querySelector('input[name=fname]');
+    const lastNameElement = document.querySelector('input[name=lname]');
+
+    console.log(firstNameElement.value);
+    console.log(lastNameElement.value);    
+}
