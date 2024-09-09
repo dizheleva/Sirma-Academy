c8c959672fb4809269b935e1c9e7d31794f4bac4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-10 00:38:26 +0300

    Added first topic

---
+++ C#/01.Basic-Syntax/Ages/Ages.cs
@@ -0,0 +1,23 @@
+﻿int age = int.Parse(Console.ReadLine());
+string result = "";
+
+switch (age)
+{
+    case >= 0 and < 3:
+        result = "baby";
+        break;
+    case <= 13:
+        result = "child";
+        break;
+    case <= 19:
+        result = "teenager";
+        break;
+    case <= 65:
+        result = "adult";
+        break;
+    case > 65:
+        result = "elder";
+        break;
+}
+
+Console.WriteLine(result);
