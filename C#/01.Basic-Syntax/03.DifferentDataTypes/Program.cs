e2f12fce27da4434de28cfe2186ccb48358721a4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:31:52 +0300

    Added solution to problem 3

---
+++ C#/01.Basic-Syntax/03.DifferentDataTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myInt = 100;
+        Console.WriteLine(myInt);
+
+        double myDouble = 3.15;
+        Console.WriteLine(myDouble);
+    }
+}
