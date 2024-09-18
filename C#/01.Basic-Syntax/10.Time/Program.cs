a9c1b40429ee3d3c36ccda5c6257edc1f7e15eb6
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:36:49 +0300

    Added solution to problem 10

---
+++ C#/01.Basic-Syntax/10.Time/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int minutes = int.Parse(Console.ReadLine());
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        string time = $"{hours:D2}:{remainingMinutes:D2}";
+
+        Console.WriteLine(time);
+    }
+}
