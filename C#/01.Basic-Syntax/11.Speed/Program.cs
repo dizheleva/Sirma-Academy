7e87dd443c2c5d6afad42a2bcfbce9c842a9a68f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:37:53 +0300

    Added solution to problem 11

---
+++ C#/01.Basic-Syntax/11.Speed/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int meters = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+        int seconds = int.Parse(Console.ReadLine());
+
+        int totalSeconds = seconds + minutes * 60 + hours * 3600;
+
+        double speed = meters / (totalSeconds * 1.00);
+        Console.WriteLine($"{speed:F6}");
+    }
+}
