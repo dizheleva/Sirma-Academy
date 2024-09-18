261b713aa426ac5630a5c11371140e7bd069aae1
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:34:46 +0300

    Added solution to problem 7

---
+++ C#/01.Basic-Syntax/07.WeatherForecast/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string city = Console.ReadLine();
+        int degrees = int.Parse(Console.ReadLine());    
+
+        Console.WriteLine($"Today in {city} it is {degrees} degrees.");
+    }
+}
