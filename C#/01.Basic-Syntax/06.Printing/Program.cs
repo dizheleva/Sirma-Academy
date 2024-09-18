90912ba20eaa56a50f4344098135ec71af526c47
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:34:07 +0300

    Added solution to problem 6

---
+++ C#/01.Basic-Syntax/06.Printing/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        char symbol = char.Parse(Console.ReadLine());
+        int integer = int.Parse(Console.ReadLine());
+        double realNumber = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"text = {text}");
+        Console.WriteLine($"symbol = {symbol}");
+        Console.WriteLine($"integer = {integer}");
+        Console.WriteLine($"real number = {realNumber}");
+
+        Console.Write($"text = {text}, ");
+        Console.Write($"symbol = {symbol}, ");
+        Console.Write($"integer = {integer}, ");
+        Console.WriteLine($"real number = {realNumber}");
+    }
+}
