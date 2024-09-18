a16f62f2bf77bc6c377d211d21832b5b70a03db4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:35:58 +0300

    Added solution to problem 9

---
+++ C#/01.Basic-Syntax/09.Calculate/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numOne = int.Parse(Console.ReadLine()); 
+        int numTwo = int.Parse(Console.ReadLine()); 
+
+        int sum = numOne + numTwo;
+        Console.WriteLine(sum);
+
+        int difference = Math.Abs(numOne - numTwo);
+        Console.WriteLine(difference);
+
+        int multiplication = numOne * numTwo;
+        Console.WriteLine(multiplication);
+
+        double average = sum / 2.0;
+        Console.WriteLine($"{average:F6}");
+    }
+}
