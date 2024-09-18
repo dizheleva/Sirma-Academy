d0e68f9e236c7dce25d8021488b8985abe9f2aa3
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-18 03:35:21 +0300

    Added solution to problem 8

---
+++ C#/01.Basic-Syntax/08.KilometersToMiles/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double kilometers = double.Parse(Console.ReadLine());
+        const double relation = 0.621371192;
+        double miles = kilometers * relation;
+
+        Console.WriteLine(miles);
+    }
+}
