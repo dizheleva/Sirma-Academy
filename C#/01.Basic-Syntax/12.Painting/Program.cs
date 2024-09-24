75475128f80183a5ac45c2324e938d1c9c17180e
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:28:44 +0300

    Added solution to problem 12

---
+++ C#/01.Basic-Syntax/12.Painting/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double red = n / 13.0;
+double yellow = 4 * red;
+double white = 2 * yellow;
+
+Console.WriteLine($"Red: {red:F4}");
+Console.WriteLine($"Yellow: {yellow:F4}");
+Console.WriteLine($"White: {white:F4}");
