5bd78d21b0c0435ebf841ec2e6438e5b8ba36030
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:29:06 +0300

    Added solution to problem 13

---
+++ C#/01.Basic-Syntax/13.Office/Program.cs
@@ -0,0 +1,8 @@
+﻿double firstCabinet = double.Parse(Console.ReadLine());
+
+double secondCabinet = 0.8 * firstCabinet;
+double thirdCabinet = 1.15 * (firstCabinet + secondCabinet);
+
+double officeCost  = firstCabinet + secondCabinet + thirdCabinet;
+
+Console.WriteLine(officeCost.ToString("F3"));
