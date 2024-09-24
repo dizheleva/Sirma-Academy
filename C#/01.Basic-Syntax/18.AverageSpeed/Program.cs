6a280e10840cdbadc5df72f7a2bee5063bc3f22d
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:30:35 +0300

    Added solution to problem 18

---
+++ C#/01.Basic-Syntax/18.AverageSpeed/Program.cs
@@ -0,0 +1,6 @@
+﻿double distance = double.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+double averageSpeed = distance / hours;
+
+Console.WriteLine(averageSpeed.ToString("F2"));
