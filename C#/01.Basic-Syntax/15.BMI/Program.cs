0ac64d8036e5b57802e46ae90b6c3779a2a2ed08
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:29:42 +0300

    Added solution to problem 15

---
+++ C#/01.Basic-Syntax/15.BMI/Program.cs
@@ -0,0 +1,6 @@
+﻿double weight = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+double bmi = weight / (height * height);
+
+Console.WriteLine(bmi.ToString("F2"));
