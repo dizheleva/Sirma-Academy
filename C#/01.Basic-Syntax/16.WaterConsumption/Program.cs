2d0cd253f3b7a457ea0d14a94a767bf33092120b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:29:59 +0300

    Added solution to problem 16

---
+++ C#/01.Basic-Syntax/16.WaterConsumption/Program.cs
@@ -0,0 +1,6 @@
+﻿int totalWeeklyConsumption = int.Parse(Console.ReadLine());
+int people = int.Parse(Console.ReadLine()); 
+
+double dailyConsumptionPerPerson = totalWeeklyConsumption / (7 * 1.00) / (people * 1.00);
+
+Console.WriteLine(dailyConsumptionPerPerson.ToString("F2"));
