f43792690725c6412bf980016febd9e995d17426
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-24 16:30:51 +0300

    Added solution to problem 19

---
+++ C#/01.Basic-Syntax/19.FuelEfficiency/Program.cs
@@ -0,0 +1,7 @@
+﻿double distance = double.Parse(Console.ReadLine());
+int fuel = int.Parse(Console.ReadLine());
+
+double efficiency = distance / fuel;
+
+// Fuel Efficiency = D / F
+Console.WriteLine(efficiency.ToString("F2"));
