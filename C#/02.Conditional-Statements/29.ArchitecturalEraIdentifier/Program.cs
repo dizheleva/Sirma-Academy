8b171ae146105e0e0b1c728ba805f71fb6b55c21
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:27:38 +0300

    Added solution to problem 29

---
+++ C#/02.Conditional-Statements/29.ArchitecturalEraIdentifier/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+        string material = Console.ReadLine();
+
+        string result = "";
+        if (year < 500 && material == "stone") 
+        {
+            result = "Ancient";
+        }
+        else if (year >= 500 && year <= 1500 && material == "stone")
+        {
+            result = "Medieval";
+        }
+        else if (year >= 1500 && year <= 1800 && material == "wood")
+        {
+            result = "Colonial";
+        }
+        else if (year >= 1800 && year <= 1900 && material == "steel")
+        {
+            result = "Industrial";
+        }
+        else if (year > 1900 && material == "steel")
+        {
+            result = "Modern";
+        }
+        else
+        {
+            result = "Uncertain";
+        }
+
+        Console.WriteLine(result);
+    }
+}
