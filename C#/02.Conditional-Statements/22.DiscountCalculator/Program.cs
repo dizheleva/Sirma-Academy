d6de294e20d23cb9f7d1f365f96215640215de50
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:26:37 +0300

    Added solution to problem 22

---
+++ C#/02.Conditional-Statements/22.DiscountCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = int.Parse(Console.ReadLine());
+        string cardStatus = Console.ReadLine();
+
+        int discount = 0;
+        switch (age)
+        {
+            case < 18:
+                discount = 10;
+                break;
+            case <= 64:
+                switch (cardStatus)
+                {
+                    case "Yes":
+                        discount = 20;
+                        break;
+                    case "No":
+                        discount = 10;
+                        break;
+                }
+                break;
+            case >= 65:
+                discount = 30;
+                break;
+        }
+
+        Console.WriteLine($"{discount}% discount");
+    }
+}
