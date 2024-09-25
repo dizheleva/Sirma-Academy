89fbb14253075ae541aaf24930d776416befe8da
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:23:25 +0300

    Added solution to problem 09

---
+++ C#/02.Conditional-Statements/09.AddressByAgeAndGender/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = int.Parse(Console.ReadLine());
+        char gender = char.Parse(Console.ReadLine());
+
+        switch (gender)
+        {
+            case 'f':
+                switch (age)
+                {
+                    case < 16:
+                        Console.WriteLine("Miss");
+                        break;
+                    default:
+                        Console.WriteLine("Ms.");
+                        break;
+                }
+                break;
+            case 'm':
+                switch (age)
+                {
+                    case < 16:
+                        Console.WriteLine("Master");
+                        break;
+                    default:
+                        Console.WriteLine("Mr.");
+                        break;
+                }
+                break;
+        }
+    }
+}
