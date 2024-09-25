b5e94e6ea9497e6f572432b9d86e03d186c2f505
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:26:28 +0300

    Added solution to problem 21

---
+++ C#/02.Conditional-Statements/21.UnivercityAdmissions/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int score = int.Parse(Console.ReadLine());
+        int extracurriculars = int.Parse(Console.ReadLine());
+
+        bool isAdmitted = true;
+
+        if (score < 80 || score < 90 && extracurriculars < 2)
+        {
+            isAdmitted = false;
+        }
+
+        Console.WriteLine(isAdmitted ? "Admitted" : "Not admitted");
+    }
+}
