70c31cf26c29b5dfaf7186f8ad0d2251aebc165f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:24:39 +0300

    Added solution to problem 17

---
+++ C#/02.Conditional-Statements/17.GradeCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int score = int.Parse(Console.ReadLine());
+
+        char grade;
+
+        if (score <= 59)
+        {
+            grade = 'F';
+        }
+        else if (score <= 69)
+        {
+            grade = 'D';
+        }
+        else if (score <= 79)
+        {
+            grade = 'C';
+        }
+        else if (score <= 89)
+        {
+            grade = 'B';
+        }
+        else
+        {
+            grade = 'A';
+        }
+        
+        Console.WriteLine(grade);
+    }
+}
