1cb3cff862de715d29dfa8c2ae813dcb8e0c19a1
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:27:31 +0300

    Added solution to problem 28

---
+++ C#/02.Conditional-Statements/28.ClimateZoneIdentifier/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double latitude = double.Parse(Console.ReadLine());
+        char direction = char.Parse(Console.ReadLine());
+
+        string result = "";
+        latitude = Math.Abs(latitude);
+        if (latitude > 66.5)
+        {
+            result = "Arctic Zone";
+        }
+        else if (latitude > 23.5 && latitude <= 66.5)
+        {
+            result = "Tempreture Zone";
+        }
+        if (latitude > 0 && latitude <= 23.5)
+        {
+            result = "Tropic Zone";
+        }
+        if (latitude == 0)
+        {
+            result = "Equator";
+        }
+
+        Console.WriteLine(result);
+    }
+}
