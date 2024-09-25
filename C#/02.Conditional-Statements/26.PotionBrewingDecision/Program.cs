bd6a3389ac0702aee392b9ed2ee2cf392b03be98
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:27:10 +0300

    Added solution to problem 26

---
+++ C#/02.Conditional-Statements/26.PotionBrewingDecision/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string firstIngredient = Console.ReadLine();
+        string secondIngredient = Console.ReadLine();
+
+        string result = "";
+        if (firstIngredient == "herbs")
+        {
+            if (secondIngredient == "water")
+            {
+                result = "Health potion";
+            }
+            else if (secondIngredient == "oil")
+            {
+                result = "Stealth potion";
+            }
+            else
+            {
+                result = "Minor stamina potion";
+            }
+        }
+        else if (firstIngredient == "berries")
+        {
+            if (secondIngredient == "sugar")
+            {
+                result = "Speed potion";
+            }
+            else
+            {
+                result = "Minor energy potion";
+            }
+        }
+        else
+        {
+            result = "No potion";
+        }
+
+        Console.WriteLine(result);
+    }
+}
