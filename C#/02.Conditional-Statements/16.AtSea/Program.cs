039aa92b0ea17271a963c65aca485a5d21aca4a0
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:24:28 +0300

    Added solution to problem 16

---
+++ C#/02.Conditional-Statements/16.AtSea/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal singleRoom = 25.00m;
+        decimal apartment = 50.00m;
+        decimal presidential = 100.00m;
+
+        int days = int.Parse(Console.ReadLine());
+        string room = Console.ReadLine();
+        string assesssment = Console.ReadLine();
+
+        decimal totalPrice = 0;
+        switch (room)
+        {
+            case "single room":
+                totalPrice = singleRoom * (days - 1);
+                break;
+            case "apartment":
+                totalPrice = apartment * (days - 1);
+                switch (days)
+                {
+                    case < 10:
+                        totalPrice = 0.7m * totalPrice;
+                        break;
+                    case <= 15:
+                        totalPrice = 0.65m * totalPrice;
+                        break;
+                    case > 15:
+                        totalPrice = 0.5m * totalPrice;
+                        break;
+                }
+                break;
+            case "presidential":
+                totalPrice = presidential * (days - 1);
+                switch (days)
+                {
+                    case < 10:
+                        totalPrice = 0.9m * totalPrice;
+                        break;
+                    case <= 15:
+                        totalPrice = 0.85m * totalPrice;
+                        break;
+                    case > 15:
+                        totalPrice = 0.8m * totalPrice;
+                        break;
+                }
+                break;
+        }
+
+        switch (assesssment)
+        {
+            case "positive":
+                totalPrice = 1.25m * totalPrice;
+                break;
+            case "negative":
+                totalPrice = 0.9m * totalPrice;
+                break;
+        }
+
+        Console.WriteLine(totalPrice.ToString("F2"));
+    }
+}
