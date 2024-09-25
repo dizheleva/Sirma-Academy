059f33e40c4e1e66472d9c681378d2fcd701bf6c
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:23:02 +0300

    Added solution to problem 07

---
+++ C#/02.Conditional-Statements/07.Speed/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double speed = double.Parse(Console.ReadLine());
+
+        string result = "";
+        switch (speed)
+        {
+            case <= 10:
+                result = "slow";
+                break;
+            case <= 60:
+                result = "average";
+                break;
+            case <= 120:
+                result = "fast";
+                break;
+            case <= 160:
+                result = "super-fast";
+                break;
+            case > 160:
+                result = "turbo-fast";
+                break;
+        }
+
+        Console.WriteLine(result);
+    }
+}
