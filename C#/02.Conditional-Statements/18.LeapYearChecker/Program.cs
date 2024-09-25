043575cc97a6370950510db01b2875bc1e61c46b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:25:06 +0300

    Added solution to problem 18

---
+++ C#/02.Conditional-Statements/18.LeapYearChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        bool isLeap = true;
+
+        if (year % 4 == 0 && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)))
+        {
+            isLeap = true;
+        }
+        else
+        {
+            isLeap = false;
+        }
+
+        string result = isLeap ? "It`s a leap year!" : "It`s not a leap year.";
+        Console.WriteLine(result);
+    }
+}
