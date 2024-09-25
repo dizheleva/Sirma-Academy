4c239a00774a866f63e1f753d792991d43d8d723
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:23:50 +0300

    Added solution to problem 12

---
+++ C#/02.Conditional-Statements/12.SimpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+
+        string operation = Console.ReadLine();
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "add":
+                result = a + b; 
+                break;
+            case "subtract":
+                result = a - b; 
+                break;
+            case "multiply":
+                result = a * b; 
+                break;
+            case "divide":
+                result = a / (b * 1.0); 
+                break;
+        }
+
+        Console.WriteLine(result.ToString("F2"));
+    }
+}
