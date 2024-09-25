23d4e57d5c3f9576ba9cf096143dc3a85393707f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:21:30 +0300

    Added second problem solution

---
+++ C#/02.Conditional-Statements/02.LargerNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberOne = int.Parse(Console.ReadLine());
+        int numberTwo = int.Parse(Console.ReadLine());
+
+        if (numberOne > numberTwo)
+        {
+            Console.WriteLine(numberOne);
+        }
+        else
+        {
+            Console.WriteLine(numberTwo);
+        }
+    }
+}
