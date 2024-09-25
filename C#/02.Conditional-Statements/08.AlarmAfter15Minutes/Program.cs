55a5e9f251fba87c3397d8963c77ee8b5d7f6266
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:23:12 +0300

    Added solution to problem 08

---
+++ C#/02.Conditional-Statements/08.AlarmAfter15Minutes/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int inputHour = int.Parse(Console.ReadLine());
+        int inputMinutes = int.Parse(Console.ReadLine());
+
+        int resultMinutes = (inputMinutes + 15) % 60;
+        int resultHour = (inputHour + (inputMinutes + 15) / 60) % 24;
+
+        Console.WriteLine($"{resultHour}:{resultMinutes:D2}");
+    }
+}
