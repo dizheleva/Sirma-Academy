c2a82df90f1520a4e3cd24b223e459df7033e28a
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:27:03 +0300

    Added solution to problem 25

---
+++ C#/02.Conditional-Statements/25.AdventureGame/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string rightHand = Console.ReadLine();
+        string leftHand = Console.ReadLine();
+
+        string result = "";
+        if (rightHand == "sword")
+        {
+            if (leftHand == "shield")
+            {
+                result = "Path to the castle";
+            }
+            else
+            {
+                result = "Path to the forest";
+            }
+        }
+        else if (rightHand == "map")
+        {
+            if (leftHand == "coins")
+            {
+                result = "Go to the town";
+            }
+            else
+            {
+                result = "Camp";
+            }
+        }
+        else
+        {
+            result = "Wander aimlessly";
+        }
+
+        Console.WriteLine(result);
+    }
+}
