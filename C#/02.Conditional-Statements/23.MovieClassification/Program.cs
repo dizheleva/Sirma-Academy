c2b84cbb082773485919a9a794b1164ac1cbb827
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:26:48 +0300

    Added solution to problem 23

---
+++ C#/02.Conditional-Statements/23.MovieClassification/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = int.Parse(Console.ReadLine());
+
+        int discount = 0;
+        switch (age)
+        {
+            case < 13:
+                Console.WriteLine("U-rated movies");
+                break;
+            case <= 17:
+                Console.WriteLine("U and PG-13 rated movies");
+                break;
+            case >= 18:
+                Console.WriteLine("All movies");
+                break;
+        }
+    }
+}
