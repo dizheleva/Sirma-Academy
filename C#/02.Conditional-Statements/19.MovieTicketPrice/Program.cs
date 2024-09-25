31b890b4d6ca8c985fc56d9bf3366c29d24caf8b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:25:26 +0300

    Added solution to problem 19

---
+++ C#/02.Conditional-Statements/19.MovieTicketPrice/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = int.Parse(Console.ReadLine());
+
+        if (age > 0 && age <= 12)
+        {
+            Console.WriteLine("$5");
+        }
+        else if (age <= 19)
+        {
+            Console.WriteLine("$8");
+        }
+        else 
+        {
+            Console.WriteLine("$10");
+        }
+    }
+}
