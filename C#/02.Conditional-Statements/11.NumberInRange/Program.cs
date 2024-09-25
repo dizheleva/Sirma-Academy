6070d311f600ec159dc7081dcb5e18133c73498f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-25 12:23:42 +0300

    Added solution to problem 11

---
+++ C#/02.Conditional-Statements/11.NumberInRange/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= -100 && number <= 100 && number != 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
