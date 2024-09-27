f14bb9c6d6a90e7cf660fc040ffb2687ec469972
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:26:59 +0300

    Added task 9

---
+++ C#/03.Loops/09.SumOfEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 0; i <= n * 2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
