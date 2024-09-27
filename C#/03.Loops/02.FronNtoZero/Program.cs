67dda5f58aef7ef93ff6b520e795495ee5898040
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:25:07 +0300

    Task 2 solution updated

---
+++ C#/03.Loops/02.FronNtoZero/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = n; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }        
+    }
+}
