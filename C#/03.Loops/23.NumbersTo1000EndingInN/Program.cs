2f20b7b6cac7d8eb944837675794b2a82829beb4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:29:19 +0300

    Added task 23

---
+++ C#/03.Loops/23.NumbersTo1000EndingInN/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int num = 1; num <= 1000; num++)
+        {
+            if (num % 10 == n)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
