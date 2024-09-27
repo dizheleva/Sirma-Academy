d0cde832f7ee5da0ccf032eb509ef92ba1c2ab1d
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:26:52 +0300

    Added task 8

---
+++ C#/03.Loops/08.MultiplicationTable/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} * {j} = {i * j}");
+            }
+        }
+    }
+}
