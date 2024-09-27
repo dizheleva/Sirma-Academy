4f03098e5ae8d88be6477311c8ffe850aeec9810
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:29:01 +0300

    Added task 21

---
+++ C#/03.Loops/21.RefactorSumOfOddNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int current = 2 * i + 1;
+            Console.WriteLine(current);
+            sum += current;
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
