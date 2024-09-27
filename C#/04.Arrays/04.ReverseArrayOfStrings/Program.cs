ca8d15d409ff79a573e7e1dc17c80de5d06785b4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:58:01 +0300

    Added solution to task 05

---
+++ C#/04.Arrays/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(",");
+
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            string currentElement = arr[i];
+            arr[i] = arr[arr.Length - 1 - i];
+            arr[arr.Length - 1 - i] = currentElement;
+        }
+
+        Console.WriteLine(String.Join(" ", arr));
+    }
+}
