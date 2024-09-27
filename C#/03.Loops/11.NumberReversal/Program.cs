734fe6ce69394ba4f0087c2dec35251f3b64a34e
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:20 +0300

    Added task 11

---
+++ C#/03.Loops/11.NumberReversal/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string reversed = "";
+
+        while (n >= 1)
+        {
+            reversed += (n % 10);
+            n = n / 10;
+        }
+
+        Console.WriteLine(reversed);
+    }
+}
