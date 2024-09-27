7cba1b8225e027fd489f7c14ff969edb00e1ab2b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:59:02 +0300

    Added solution to task 09

---
+++ C#/04.Arrays/09.PrintN-thElementFromArray/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(",");
+        int n = int.Parse(Console.ReadLine());
+
+        int outputArrLength = (int)Math.Ceiling(arr.Length / (n * 1.0));
+        string[] outputArr = new string[outputArrLength];
+        int step = 0;
+        for (int i = 0; i < outputArrLength; i ++)
+        {
+            outputArr[i] = arr[step];
+            step += n;
+        }
+
+        Console.WriteLine(String.Join(' ', outputArr));
+    }
+}
