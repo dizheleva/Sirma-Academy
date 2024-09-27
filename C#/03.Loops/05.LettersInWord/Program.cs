0c088c09c737478f5a5af53474eb148dab3f71c1
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:26:29 +0300

    Added task 5

---
+++ C#/03.Loops/05.LettersInWord/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+
+        for (int i = 0; i < word.Length; i ++)
+        {
+            Console.WriteLine(word[i]);
+        }
+    }
+}
