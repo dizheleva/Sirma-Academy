edf8fcbdfb046128c38c3766fc8519e587bb3409
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:30:51 +0300

    Added task 33

---
+++ C#/03.Loops/33.TreePattern/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        char symbol = '*';
+        string row;
+        for (int r = 1; r <= n; r++)
+        {
+            row = "";
+
+            for (int c = 1; c <= r * 2 - 1; c++)
+            {
+                while (row.Length < n - r)
+                {
+                    row += ' ';
+                }
+
+                row += symbol;
+            }
+
+            Console.WriteLine(row);
+        }
+
+        row = "";
+        for (int s = 0; s < n - 1; s++)
+        {
+            row += ' ';
+        }
+        row += symbol;
+
+        Console.WriteLine(row);
+    }
+}
