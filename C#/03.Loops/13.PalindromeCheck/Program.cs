d2377ac173397304244f60d5b8af508eb67d73de
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:43 +0300

    Added task 13

---
+++ C#/03.Loops/13.PalindromeCheck/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        string mirrorWord = "";
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            mirrorWord += word[i];
+        }
+
+        bool isPalindrome = word == mirrorWord;
+        Console.WriteLine(isPalindrome.ToString().ToLower());
+    }
+}
