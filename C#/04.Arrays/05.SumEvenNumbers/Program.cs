c9820c2ddb0968d4693960d48392967058b0a131
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:58:22 +0300

    Added solution to task 05

---
+++ C#/04.Arrays/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(",");
+
+        int sum = 0;
+
+        foreach (string element in arr)
+        {
+            int number = int.Parse(element);
+
+            if (number % 2 == 0)
+            {
+                sum += number;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
