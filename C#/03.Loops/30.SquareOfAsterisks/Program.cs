7634e1b1ec185b57413a80e094020c2783718f39
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:30:24 +0300

    Added task 30

---
+++ C#/03.Loops/30.SquareOfAsterisks/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int r = 0; r < n; r++)
+        {
+            string row = "";
+
+            for (int c = 0; c < n; c++)
+            {
+                row += "* ";
+            }
+
+            Console.WriteLine(row.Trim());
+        }
+    }
+}
