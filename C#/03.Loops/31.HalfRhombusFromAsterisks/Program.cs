4bc731a7cc88bdf428c81733f198b4929ca79121
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:30:37 +0300

    Added task 31

---
+++ C#/03.Loops/31.HalfRhombusFromAsterisks/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int rows = n * 2 - 1;
+        string symbol = "* ";
+
+        for (int r = 1; r <= rows; r++)
+        {
+            string row = "";
+
+            int colEnd = r <= n ? r : rows - r + 1;
+
+            for (int c = 1; c <= colEnd; c++)
+            {
+                row += symbol;
+            }
+
+            Console.WriteLine(row.Trim());
+        }
+    }
+}
