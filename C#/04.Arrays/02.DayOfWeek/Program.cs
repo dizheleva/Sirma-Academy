932420b4bb6ed4a763645336ee316a7dd13d4f99
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:57:35 +0300

    Added solution to task 02

---
+++ C#/04.Arrays/02.DayOfWeek/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] daysOfWeek = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        int number = int.Parse(Console.ReadLine());
+
+        if (number - 1 >= 0 && number - 1 < daysOfWeek.Length)
+        {
+            Console.WriteLine(daysOfWeek[number - 1]);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Day!");
+        }
+    }
+}
