0af3c33bbfa881a9000d656a26b87e64e5de8d8a
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:31:09 +0300

    Added task 35

---
+++ C#/03.Loops/35.ChristmasTree/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string symbol;
+       
+        for (int r = 0; r <= n; r++)
+        {
+            string row = "";
+
+            for (int c = 0; c < r * 2 + 1; c++)
+            {
+                while (row.Length < n - r)
+                {
+                    row += ' ';
+                }
+
+                symbol = c == r ? " | " : "*";                
+                row += symbol;
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
