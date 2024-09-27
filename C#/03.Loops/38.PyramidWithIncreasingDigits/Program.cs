a9265f9162a62f6d4620e73e528dab4d8d4aa3b1
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:31:31 +0300

    Added task 38

---
+++ C#/03.Loops/38.PyramidWithIncreasingDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int r = 1; r <= n; r++)
+        {
+            string row = "";
+
+            int rowCounter = r;
+            for (int c = n - r + 1; c < n + r; c++)
+            {
+                while (row.Length < n - r + 1)
+                {
+                    row += ' ';
+                }
+
+                row += rowCounter;
+                rowCounter += c >= n ? -1 : 1;
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
