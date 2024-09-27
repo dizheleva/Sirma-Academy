40c592e071ed35fcd9f4bacf4d62ddacf2ce3849
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:58:33 +0300

    Added solution to task 06

---
+++ C#/04.Arrays/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+            if (arr[i] % 2 == 0)
+            {
+                evenSum += arr[i];
+            }
+            else
+            {
+                oddSum += arr[i];
+            }
+        }
+
+        int result = evenSum - oddSum;
+        Console.WriteLine(result);
+    }
+}
