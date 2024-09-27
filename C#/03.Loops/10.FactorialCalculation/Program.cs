417294ea8d985db772b0ef62345a841bdbf5e30b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:11 +0300

    Added task 10

---
+++ C#/03.Loops/10.FactorialCalculation/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine(factorial);
+    }
+}
