23d474f8e62e62b9eb4d9f9142e3c16bd9db8e8c
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:58:42 +0300

    Added solution to task 07

---
+++ C#/04.Arrays/07.EqualArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] firstArr = Console.ReadLine().Split(",");
+        string[] secondArr = Console.ReadLine().Split(",");
+
+        int sum = 0;
+        bool isIdentical = true;
+        int index;
+
+        for (index = 0; index < firstArr.Length; index++)
+        {
+            if (firstArr[index] == secondArr[index])
+            {
+                sum += int.Parse(firstArr[index]);
+            }
+            else
+            {
+                isIdentical = false;
+                break;
+            }
+        }
+
+        string output = isIdentical 
+            ? $"Arrays are identical. Sum: {sum}"
+            : $"Arrays are not identical. Found difference at {index} index";
+
+        Console.WriteLine(output);
+    }
+}
