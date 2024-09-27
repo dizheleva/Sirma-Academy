d06ac34cbe66c7e27f14e9647523f36ba23e8bf2
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:59:19 +0300

    Added solution to task 11

---
+++ C#/04.Arrays/11.RotateArray/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(",");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] outputArr = new string[arr.Length];
+        int movedPositions = n % arr.Length;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int currentIndex = i - movedPositions < 0 ? arr.Length + (i - movedPositions) : i - movedPositions;
+            outputArr[i] = arr[currentIndex];
+        }
+
+        Console.WriteLine(String.Join(' ', outputArr));
+    }
+}
