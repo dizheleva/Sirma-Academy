0984f36eca0798cb5044586c63d228f24bfa6dcb
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:57:27 +0300

    Added solution to task 01

---
+++ C#/04.Arrays/01.SumFirstLastArrayElement/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int firstNumber = arr[0];
+        int lastNumber = arr[arr.Length - 1];
+
+        int result = firstNumber + lastNumber;
+        Console.WriteLine(result);
+    }
+}
