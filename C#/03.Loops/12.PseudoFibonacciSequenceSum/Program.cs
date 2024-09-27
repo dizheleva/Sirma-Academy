e3a3ad2c195897eee92c744a9a34786ab4614439
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:33 +0300

    Added task 12

---
+++ C#/03.Loops/12.PseudoFibonacciSequenceSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i <= n; i++)
+        {
+            sum += a;
+            int step = a + b;
+            a = b;
+            b = step;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
