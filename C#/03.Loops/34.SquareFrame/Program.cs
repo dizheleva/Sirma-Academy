53e59bf534a26689e80e3b03262a59b0726a9db9
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:31:00 +0300

    Added task 34

---
+++ C#/03.Loops/34.SquareFrame/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int r = 1; r <= n; r++)
+        {
+            string row = "";
+
+            for (int c = 1; c <= n; c++)
+            {
+                char symbol;
+                
+                if ((r == 1 || r == n) && (c == 1 || c == n))
+                {
+                    symbol = '+';
+                }
+                else if (c == 1 || c == n)
+                {
+                    symbol = '|';
+                }
+                else
+                {
+                    symbol = '-';
+                }
+
+                row += symbol;
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
