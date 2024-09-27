7910c5bfefbaa087a6e122b5495a1059a8c7af1f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:32:08 +0300

    Added task 42

---
+++ C#/03.Loops/42.LeftArrowPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string row;
+
+        for (int r = 1; r < n; r++)
+        {
+            row = "";
+
+            while (row.Length < n - 1 - r)
+            {
+                row += ' ';
+            }
+
+            for (int c = 1; c <= r; c++)
+            {
+                row += '*';
+            }
+
+            Console.WriteLine(row);
+        }
+
+        for (int r = 1; r < n - 1; r++)
+        {
+            row = "";
+
+            while (row.Length < r)
+            {
+                row += ' ';
+            }
+
+            for (int c = 1; c <= n - 1 - r; c++)
+            {
+                row += '*';
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
