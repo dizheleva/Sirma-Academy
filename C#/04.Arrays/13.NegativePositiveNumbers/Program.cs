15ddfde5bfffcb525f8a99613ec6a860fde1b684
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:59:34 +0300

    Added solution to task 13

---
+++ C#/04.Arrays/13.NegativePositiveNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+        int[] outputArr = [];
+      
+        foreach (int element in arr)
+        {          
+            int[] currentArr = new int[outputArr.Length + 1];
+            if (currentArr.Length == 0 || element >= 0)
+            {
+                for (int i = 0; i < currentArr.Length - 1; i++)
+                {
+                    currentArr[i] = outputArr[i];
+                }
+                currentArr[currentArr.Length - 1] = element;
+            }
+            else
+            {
+                currentArr[0] = element;
+                for (int i = 1; i < currentArr.Length; i++)
+                {
+                    currentArr[i] = outputArr[i - 1];
+                }
+            }
+            outputArr = currentArr;
+        }
+
+        foreach (int currentElement in outputArr)
+        {
+            Console.WriteLine(currentElement);
+        }
+    }
+}
