4489f1e25b150ccfd2632e506b58b5136ace59a4
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:28:13 +0300

    Added task 16

---
+++ C#/03.Loops/16.HollowRectanglePattern/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        for (int r = 0; r < n; r++)
+        {
+            string row = "";
+
+            for (int c = 0; c < m; c++)
+            {
+                char symbol;
+                bool isBorderRow = r == 0 || r == n - 1;
+                bool isBorderCol = c == 0 || c == m - 1;
+
+                if (isBorderRow || isBorderCol)
+                {
+                    symbol = '*';
+                }
+                else
+                {
+                    symbol = ' ';
+                }
+
+                row += symbol;
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
