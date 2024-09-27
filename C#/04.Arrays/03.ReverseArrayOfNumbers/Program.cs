33d46e7d94d6af61163fb0dbaae13f9041281466
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:57:43 +0300

    Added solution to task 03

---
+++ C#/04.Arrays/03.ReverseArrayOfNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int[] reversedArr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            reversedArr[i] = arr[n - i - 1];
+        }
+
+        Console.WriteLine(String.Join(" ", reversedArr));
+    }
+}
