6fa1684309341e7b1c50ede27619eb703e7e4d01
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:30:44 +0300

    Added task 32

---
+++ C#/03.Loops/32.RhombusFromAsterisks/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int rows = n * 2 - 1;
+        string symbol = "* ";
+
+        for (int r = 0; r < rows; r++)
+        {
+            string row = "";
+
+            int colEnd = r < n ? r + 1 : rows - r;
+
+            for (int c = 0; c < colEnd; c++)
+            {
+                int spacesCount = Math.Abs(n - (r + 1));                
+
+                while (row.Length < spacesCount)
+                {
+                    row += ' ';
+                }
+
+                row += symbol;
+            }            
+
+            Console.WriteLine(row);
+        }
+    }
+}
