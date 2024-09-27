2ed02ea13ecb765f412968c13979b5539f4302c3
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:29:28 +0300

    Added task 24

---
+++ C#/03.Loops/24.Encoding/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            int charCode = digit + 33;
+
+            string line = "";
+            if (digit == 0)
+            {
+                line = "ZERO";
+            }
+            else
+            {
+                for (int i = 0; i < digit; i++)
+                {
+                    line += (char)charCode;
+                }
+            }
+
+            Console.WriteLine(line);
+            number /= 10;
+        }
+    }
+}
