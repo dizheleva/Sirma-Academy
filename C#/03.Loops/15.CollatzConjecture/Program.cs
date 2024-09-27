52ccccd62c60e74f9c45c854c6f5ba963fcc774b
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:58 +0300

    Added task 15

---
+++ C#/03.Loops/15.CollatzConjecture/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string result = $"{n}";
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            else
+            {
+                n = n * 3 + 1;
+            }
+            result += $" {n}";
+        }
+        
+        Console.WriteLine(result);
+    }
+}
