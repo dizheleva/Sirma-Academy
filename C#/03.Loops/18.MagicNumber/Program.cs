3d8fcaeebe1854e5ac76d2a8fd09042de6d287d3
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:28:34 +0300

    Added task 18

---
+++ C#/03.Loops/18.MagicNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int bottom = int.Parse(Console.ReadLine());
+        int top = int.Parse(Console.ReadLine());
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        string result = "";
+        int counter = 0;
+        
+        for (int i = bottom; i <= top; i++)
+        {
+            bool isCombination = false;
+
+            for (int j = bottom; j <= top; j++)
+            {
+                isCombination = i + j == magicNumber;
+                counter++;
+                if (isCombination)
+                {
+                    result = $"Combination {counter} - ({i} + {j} = {magicNumber})";
+                    break;
+                }
+                else
+                {
+                    result = $"{counter} combinations - neither equals {magicNumber}";
+                }
+            }
+
+            if (isCombination)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
