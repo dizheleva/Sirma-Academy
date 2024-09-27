c307def07564443694a3062f6390d41e578aefa7
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:58:52 +0300

    Added solution to task 08

---
+++ C#/04.Arrays/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        while (arr.Length > 1)
+        {
+            int[] condensedArray = new int[arr.Length - 1];
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                condensedArray[i] = arr[i] + arr[i + 1];
+            }
+
+            arr = condensedArray;
+        }
+
+        Console.WriteLine(arr[0]);
+    }
+}
