da7f905c19f4acb838ab0b9525bed33b608671a9
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:29:11 +0300

    Added task 22

---
+++ C#/03.Loops/22.NumbersTo1000EendingIn7/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int num = 1; num <= 1000; num++)
+        {
+            if (num % 10 == 7)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
