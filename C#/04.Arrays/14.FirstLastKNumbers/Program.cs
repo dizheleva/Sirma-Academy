2094ec9a1c6719dca6cf85dea54177fadd6e918c
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:59:44 +0300

    Added solution to task 14

---
+++ C#/04.Arrays/14.FirstLastKNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int k = int.Parse(Console.ReadLine());
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int[] firstNumbersArr = new int[k];
+        int[] lastNumbersArr = new int[k];
+
+        for (int i = 0; i < k; i++)
+        {
+            firstNumbersArr[i] = arr[i];
+            lastNumbersArr[k - i - 1] = arr[arr.Length - 1 - i];
+        }
+
+        Console.WriteLine(String.Join(' ', firstNumbersArr));
+        Console.WriteLine(String.Join(' ', lastNumbersArr));
+    }
+}
