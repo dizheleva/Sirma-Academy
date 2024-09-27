1102c06947db946c977fee308537e48c42b894b0
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:31:51 +0300

    Added task 40

---
+++ C#/03.Loops/40.StaircasePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string row = "";
+
+        for (int r = 1; r <= n; r++)
+        {
+            int rowLength = row.Length - 1 + r;
+            int hashStart = rowLength - r;
+
+            row = "";
+
+            for (int c = 0; c < rowLength; c++)
+            {
+                row += c < hashStart ? ' ' : '#';
+            }
+
+            Console.WriteLine(row);
+        }
+    }
+}
