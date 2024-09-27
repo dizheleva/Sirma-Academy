21b10dfdab77374b59ba29fd4a60a2a71c22dc65
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:32:55 +0300

    Added task 47

---
+++ C#/03.Loops/47.SpecialNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1111; i <= 9999; i++)
+        {
+
+            bool isSpecial = true;
+
+            int currentNumber = i;
+            while (currentNumber >= 1)
+            {
+                int divider = currentNumber % 10;
+                if (divider == 0 || n % divider != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+
+                currentNumber = (int)Math.Truncate(currentNumber / 10.0);
+            }
+
+            if (isSpecial)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
