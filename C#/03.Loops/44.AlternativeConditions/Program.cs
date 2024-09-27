3544d941c2296e5236da3645000b83cd03bce056
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:32:26 +0300

    Added task 44

---
+++ C#/03.Loops/44.AlternativeConditions/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int currentNum = 0;
+
+        for (int r = 1; currentNum < n; r++)
+        {
+            string output = "";
+
+            int numbers = n;
+            int rowCount = 0;
+            while (numbers > 0)
+            {
+                rowCount++;
+                numbers -= rowCount;
+            }
+
+            for (int s = 0; s < rowCount - r; s++)
+            {
+                output += ' ';
+            }
+
+            for (int c = 0; c < r && currentNum < n; c++)
+            {
+                currentNum++;
+                output += $" {currentNum}";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
