567f75b172ffd45746738e1cad6dabc32c698207
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:30:15 +0300

    Added task 29

---
+++ C#/03.Loops/29.UniqueCodes/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int firstLimit = int.Parse(Console.ReadLine());
+        int secondLimit = int.Parse(Console.ReadLine());
+        int thirdLimit = int.Parse(Console.ReadLine());
+
+        for (int firstDigit = 1; firstDigit <= firstLimit; firstDigit++)
+        {
+            if (firstDigit % 2 == 0)
+            {
+                if (secondLimit >= 2 && secondLimit <= 7)
+                {
+                    for (int secondDigit = 2; secondDigit <= secondLimit; secondDigit++)
+                    {
+                        bool isPrime = true;
+                        for (int i = 2; i < secondDigit; i++)
+                        {
+                            if (secondDigit % i == 0)
+                            {
+                                isPrime = false;
+                            }
+
+                        }
+
+                        if (isPrime)
+                        {
+                            for (int thirdDigit = 1; thirdDigit <= thirdLimit; thirdDigit++)
+                            {
+                                if (thirdDigit % 2 == 0)
+                                {
+                                    Console.WriteLine($"{firstDigit}{secondDigit}{thirdDigit}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
