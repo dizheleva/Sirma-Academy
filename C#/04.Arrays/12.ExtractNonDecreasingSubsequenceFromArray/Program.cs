89c8b19b834128e6d41bf8e693a01dc821556404
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 02:59:26 +0300

    Added solution to task 12

---
+++ C#/04.Arrays/12.ExtractNonDecreasingSubsequenceFromArray/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int[] outputArr = [];
+
+        foreach (int element in arr)
+        {
+            int currentArrLength = outputArr.Length;
+            if (currentArrLength == 0 || element >= outputArr[outputArr.Length - 1])
+            {
+                currentArrLength++;
+                int[] currentArr = new int[currentArrLength];
+
+                for (int i = 0; i < currentArrLength - 1; i++)
+                {
+                    currentArr[i] = outputArr[i];
+                }
+
+                currentArr[currentArrLength - 1] = element;
+                outputArr = currentArr;
+            }
+        }
+
+        Console.WriteLine(String.Join(' ', outputArr));
+    }
+}
