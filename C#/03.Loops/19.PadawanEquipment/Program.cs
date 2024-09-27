90caf959e7e2ee3dff2765d78a8064178ffa9974
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:28:44 +0300

    Added task 19

---
+++ C#/03.Loops/19.PadawanEquipment/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double money = double.Parse(Console.ReadLine());
+        int students = int.Parse(Console.ReadLine());
+        double saberPrice = double.Parse(Console.ReadLine());
+        double robePrice = double.Parse(Console.ReadLine());
+        double beltPrice = double.Parse(Console.ReadLine());
+
+        double sabersCost = Math.Ceiling(1.1 * students) * saberPrice;
+        double robesCost = students * robePrice;
+        double beltsCost = (students - students / 6) * beltPrice;
+        double totalCost = sabersCost + robesCost + beltsCost;
+
+        string result;
+        if (money >= totalCost)
+        {
+            result = $"The money is enough - it would cost {totalCost:F2}lv.";
+        }
+        else
+        {
+            result = $"George Lucas will need {(totalCost - money):F2}lv more.";
+        }
+
+        Console.WriteLine(result);
+    }
+}
