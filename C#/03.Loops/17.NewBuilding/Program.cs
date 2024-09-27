d64d070f42f410992821d78184339156903f69b0
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:28:24 +0300

    Added task 17

---
+++ C#/03.Loops/17.NewBuilding/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int floors = int.Parse(Console.ReadLine());
+        int aparatments = int.Parse(Console.ReadLine());
+
+        for (int f = floors; f > 0; f--)
+        {
+            string floor = "";
+
+            for (int a = 0; a < aparatments; a++)
+            {
+                char type;
+
+                if (f == floors)
+                {
+                    type = 'L';
+                }
+                else if (f % 2 == 0)
+                {
+                    type = 'O';
+                }
+                else
+                {
+                    type = 'A';
+                }
+
+                floor += $"{type}{f}{a} ";
+            }
+
+            Console.WriteLine(floor.Trim());
+        }
+    }
+}
