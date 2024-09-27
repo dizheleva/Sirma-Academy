a05ab53334337faaf2a67d689f34caafd62ac400
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:27:50 +0300

    Added task 14

---
+++ C#/03.Loops/14.ArmstrongNumberCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string textNumber = n.ToString();
+        int digitsCount = textNumber.Length;
+        double result = 0;
+
+        for (int i = 0; i < digitsCount; i++)
+        {
+            int digit = int.Parse(textNumber[i].ToString());
+            result += Math.Pow(digit, digitsCount);
+        }
+
+        bool isArmstrong = result == n;
+
+        // Console.WriteLine(isArmstrong.ToString().ToLower());
+        Console.WriteLine(isArmstrong ? "Armstrong" : "Not Armstrong");
+    }
+}
