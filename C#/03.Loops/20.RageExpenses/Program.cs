707773b773dd11687a4e4f49bb81f1ba4b193519
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:28:54 +0300

    Added task 20

---
+++ C#/03.Loops/20.RageExpenses/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int lostGames = int.Parse(Console.ReadLine());
+        double headsetPrice = double.Parse(Console.ReadLine());
+        double mousePrice = double.Parse(Console.ReadLine());
+        double keyboardPrice = double.Parse(Console.ReadLine());
+        double displayPrice = double.Parse(Console.ReadLine());
+
+        double headsetsCost = 0;
+        double mousesCost = 0;
+        double keyboardsCost = 0;
+        double displaysCost = 0;
+
+        int keyboardsCounter = 0;
+        for (int game = 1; game <= lostGames; game++)
+        {
+
+            if (game % 2 == 0)
+            {
+                headsetsCost += headsetPrice;
+            }
+
+            if (game % 3 == 0)
+            {
+                mousesCost += mousePrice;
+            }
+
+            if (game % 2 == 0 && game % 3 == 0)
+            {
+                keyboardsCost += keyboardPrice;
+                keyboardsCounter++;
+
+                if (keyboardsCounter % 2 == 0)
+                {
+                    displaysCost += displayPrice;
+                    keyboardsCounter = 0;
+                }
+            }
+        }
+
+        double totalCost = headsetsCost + mousesCost + keyboardsCost + displaysCost;
+        Console.WriteLine($"Rage expenses: {totalCost:F2} lv.");
+    }
+}
