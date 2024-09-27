c591dda99660a6da1c5562e972f1edb22312540c
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-27 04:32:18 +0300

    Added task 43

---
+++ C#/03.Loops/43.PyramidOfNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int currentNum = 1;
+        int line = 1;
+        while (true)
+        {
+            string output = "";
+
+            bool isOver = false;
+            for (int i = 1; i <= line; i++)
+            {
+                output += $"{currentNum} ";
+                if (currentNum == n)
+                {
+                    isOver = true;
+                    break;
+                }
+                currentNum++;
+            }
+
+            Console.WriteLine(output.Trim());
+
+            line++;
+            if (isOver)
+            {
+                break;
+            }
+        }
+    }
+}
