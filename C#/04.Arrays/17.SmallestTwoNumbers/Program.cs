6bd25bd2e27c595d88465a0e75b01d0d67d1b38c
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 03:00:10 +0300

    Added solution to task 17

---
+++ C#/04.Arrays/17.SmallestTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+
+        int[] outputArr = new int[2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            int smallest = arr.Min();
+            outputArr[i] = smallest;
+            int smallestIndex = Array.IndexOf(arr, smallest);
+            arr[smallestIndex] = Int32.MaxValue;
+        }
+
+        Console.WriteLine(String.Join(' ', outputArr));
+    }
+}
