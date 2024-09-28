5eb05820211fe0bb66b38d1c94734f3d30885e20
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-09-28 03:00:19 +0300

    Added solution to task 18

---
+++ C#/04.Arrays/18.ListOfProducts/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(", ");
+
+        arr = arr.OrderBy(e => e).ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}.{arr[i]}");
+        }
+    }
+}
