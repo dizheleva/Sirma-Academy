79649d341bbeb24359e76f5882472256199b620f
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-11-03 14:40:35 +0200

    05Classes solutions added

---
+++ C#/05.Classes/05.ClassStorage/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace _05.ClassStorage
+{    public class Product(string name, decimal price, int quantity)
+    {
+        public string Name { get; set; } = name;
+        public decimal Price { get; set; } = price;
+        public int Quantity { get; set; } = quantity;
+    }
+}
