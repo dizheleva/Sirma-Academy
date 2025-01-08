1549a3e0d797d3a8f69e23be1e21afd0a9924a7d
Dilyana Zheleva <di.zheleva89@gmail.com>
2025-01-08 03:34:29 +0200

    Update SQLIntroduction-Exercise.sql

--- C#/25.SQLIntroduction/SQLIntroduction-Exercise.sql
+++ C#/25.SQLIntroduction/SQLIntroduction-Exercise.sql
@@ -1,4 +1,4 @@
---1.Creating a Database and Table------------------------
+---1.Creating a Database and Table------------------------
 
 CREATE DATABASE School
 
@@ -13,7 +13,7 @@ Age INT,
 Grade VARCHAR(10)
 )
 
---2.Inserting Data------------------------------------------------
+---2.Inserting Data------------------------------------------------
 
 INSERT INTO Students(StudentID, FirstName, LastName, Age, Grade)
 VALUES
@@ -23,38 +23,40 @@ VALUES
 		(4, 'Said', 'Ali', 16, '10th'),
 		(5, 'Harry', 'Potter', 14, '10th')
 
---3.Querying Data-------------------------------------------------
+---3.Querying Data-------------------------------------------------
 
 SELECT * FROM Students
 SELECT FirstName, Grade FROM Students
 
---4.Updating Data-------------------------------------------------
+---4.Updating Data-------------------------------------------------
 
 UPDATE Students
 SET Grade = '11th'
 WHERE StudentID = 1
 
---5.Deleting Data-------------------------------------------------
+SELECT * FROM Students
+
+---5.Deleting Data-------------------------------------------------
 
 DELETE FROM Students
 WHERE StudentID = 1
 
---6.Filtering Rows------------------------------------------------
+---6.Filtering Rows------------------------------------------------
 
 SELECT * FROM Students
 WHERE Grade = '10th' AND Age BETWEEN 14 AND 16
 
---7.Sorting Results-----------------------------------------------
+---7.Sorting Results-----------------------------------------------
 
 SELECT * FROM Students
 ORDER BY LastName ASC
 
---8.Using Aliases-------------------------------------------------
+---8.Using Aliases-------------------------------------------------
 
 SELECT CONCAT(FirstName, ' ', LastName) 
 AS FullName, Grade FROM Students
 
---9.Advanced Insertion--------------------------------------------
+---9.Advanced Insertion--------------------------------------------
 
 CREATE TABLE Graduates
 (
@@ -69,13 +71,17 @@ INSERT INTO Graduates
 SELECT * FROM Students
 WHERE Grade = '12th'
 
---10.View Creation------------------------------------------------
+SELECT * FROM Graduates
+
+---10.View Creation------------------------------------------------
 
 CREATE VIEW v_StudentsByGrade AS
 SELECT CONCAT(FirstName, ' ', LastName) AS FullName, Grade
 FROM Students
 
---11.Exploration of SQL Functions (Optional)----------------------
+SELECT * FROM v_StudentsByGrade
+
+---11.Exploration of SQL Functions (Optional)----------------------
 
 SELECT COUNT(*)
 FROM Students
