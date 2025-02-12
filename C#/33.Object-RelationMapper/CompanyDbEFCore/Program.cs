9ac9b9a462ca56bcdc75a46c4eb5604357e73202
Dilyana Zheleva <di.zheleva89@gmail.com>
2025-02-12 02:57:40 +0200

    LINQ practice added

--- C#/33.Object-RelationMapper/CompanyDbEFCore/Program.cs
+++ C#/33.Object-RelationMapper/CompanyDbEFCore/Program.cs
@@ -6,45 +6,94 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        using (var dbContext = new CompanyDbContext())
-        {
-            dbContext.Database.EnsureCreated();
-            //dbContext.Database.Migrate(); //use migrations for changing db:
-            //to migrate database:
-            // -> Open NuGet Manager Console
-            // -> Run: "Enable-Migrations"
-            // -> Run: "Add-Migration InitialCreate"
-            // -> Run: "Update-Database"
+        //using var dbContext = new CompanyDbContext();
 
-            // for next migrations repeat last 2 steps
+        //dbContext.Database.EnsureCreated();
+        //dbContext.Database.Migrate(); //use migrations for changing db:
+        //to migrate database:
+        // -> Open NuGet Manager Console
+        // -> Run: "Enable-Migrations"
+        // -> Run: "Add-Migration InitialCreate"
+        // -> Run: "Update-Database"
 
-            var newDepartment = new Department
+        // for next migrations repeat last 2 steps
+
+        //var newDepartment = new Department
+        //{
+        //    Name = "IT",
+        //    Description = "Developers department"
+        //};
+
+        //dbContext.Departments.Add(newDepartment);
+
+        //var departments = dbContext.Departments
+        //    .Where(d => d.Name == "IT")
+        //    .Select(d => d)
+        //    .OrderBy(d => d.Name)
+        //    .ThenBy(d => d.Description)
+        //    .Include(d => d.Employee)  // Joins Employees table
+        //    .ToList();
+
+        //Console.WriteLine(departments.Count);
+
+        //var departmentsQuery = from department in dbContext.Departments
+        //                       where department.Name == "IT"
+        //                       orderby department.Description
+        //                       select department;
+
+        //var departmentsList = departmentsQuery.ToList();
+
+        //dbContext.SaveChanges();
+
+        /******************************************************************************/
+        /** LINQ operations **/
+
+        var dbContext = new CompanyDbContext();
+
+        var employeesTransactions = dbContext.Employees
+            .Include(e => e.Transactions)
+            .Select(e => new
             {
-                Name = "IT",
-                Description = "Developers department"
-            };
+                Employee = e.FirstName,
+                TransactionAmount = e.Transactions
+            })
+            .ToList();
 
-            dbContext.Departments.Add(newDepartment);
+        var employeesQuery = (from employee in dbContext.Employees
+                              join transaction in dbContext.Transactions on employee.Id equals transaction.EmployeeId
+                              select new
+                              {
+                                  Employee = employee.FirstName + " " + employee.LastName,
+                                  TransactionAmount = new
+                                  {
+                                      Date = transaction.TransactionDate,
+                                      Amount = transaction.Amount
+                                  }
+                              }).ToList();
 
-            //var departments = dbContext.Departments
-            //    .Where(d => d.Name == "IT")
-            //    .Select(d => d)
-            //    .OrderBy(d => d.Name)
-            //    .ThenBy(d => d.Description)
-            //    .Include(d => d.Employee)  // Joins Employees table
-            //    .ToList();
-
-            //Console.WriteLine(departments.Count);
-
-            //var departmentsQuery = from department in dbContext.Departments
-            //                       where department.Name == "IT"
-            //                       orderby department.Description
-            //                       select department;
-
-            //var departmentsList = departmentsQuery.ToList();
-
-            dbContext.SaveChanges();
+        foreach (var transaction in employeesQuery)
+        {
+            Console.WriteLine(transaction.ToString());
         }
-        
+
+        foreach (var transaction in employeesTransactions)
+        {
+            Console.WriteLine(transaction.ToString());
+        }
+
+        var employeesWithDepartments = 
+            dbContext.Employees.Join(
+            dbContext.Departments,
+            (e => e.DepartmentId),
+            (d => d.Id),
+            (e, d) => new
+            {
+                Employee = e.FirstName + " " + e.LastName,
+                e.Position,
+                Department = d.Name
+            }
+            ).ToList();
+
+
     }
 }
